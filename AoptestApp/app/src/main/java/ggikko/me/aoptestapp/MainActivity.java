3d394e9d711506fb783179e0a04e2d6b43ba14c1
Jihong Park <ggikko@hotmail.com>
2016-05-18 12:55:12 +0900

    basic annotation

--- AoptestApp/app/src/main/java/ggikko/me/aoptestapp/MainActivity.java
+++ AoptestApp/app/src/main/java/ggikko/me/aoptestapp/MainActivity.java
@@ -3,6 +3,7 @@ package ggikko.me.aoptestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +11,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @SuppressWarnings("depreacated") /** Warnings are not only suppressed for the annotated element but also for all program elements contained in that element. */
+    @Deprecated /** Compilers produce a warning if a deprecated program element is used. */
+    public void deprecatedAnotation(){
+    }
+
+    /** Compilers produce an error if a method annotated with @Override does not actually override a method in a superclass.*/
+    @Override
+    public void setTheme(int resid) {
+        super.setTheme(resid);
+    }
+
+
 }
